957d32efe13f344a66a2e5580307e512e65c446c
akats16 <akats@usc.edu>
2020-09-12 08:58:08 -0700

    fixed js

--- js/menu.js
+++ js/menu.js
@@ -4,7 +4,7 @@ jQuery(document).ready(function($){
 
 	//primary navigation slide-in effect
 	if($(window).width() > MQL) {
-		var headerHeight = $('.akats16-header').height();
+		var headerHeight = $('.box-header').height();
 		$(window).on('scroll',
 		{
 	        previousTop: 0
@@ -14,32 +14,32 @@ jQuery(document).ready(function($){
 		    //check if user is scrolling up
 		    if (currentTop < this.previousTop ) {
 		    	//if scrolling up...
-		    	if (currentTop > 0 && $('.akats16-header').hasClass('is-fixed')) {
-		    		$('.akats16-header').addClass('is-visible');
+		    	if (currentTop > 0 && $('.box-header').hasClass('is-fixed')) {
+		    		$('.box-header').addClass('is-visible');
 		    	} else {
-		    		$('.akats16-header').removeClass('is-visible is-fixed');
+		    		$('.box-header').removeClass('is-visible is-fixed');
 		    	}
 		    } else {
 		    	//if scrolling down...
-		    	$('.akats16-header').removeClass('is-visible');
-		    	if( currentTop > headerHeight && !$('.akats16-header').hasClass('is-fixed')) $('.akats16-header').addClass('is-fixed');
+		    	$('.box-header').removeClass('is-visible');
+		    	if( currentTop > headerHeight && !$('.box-header').hasClass('is-fixed')) $('.box-header').addClass('is-fixed');
 		    }
 		    this.previousTop = currentTop;
 		});
 	}
 
 	//open/close primary navigation
-	$('.akats16-primary-nav-trigger').on('click', function(){
-		$('.akats16-menu-icon').toggleClass('is-clicked');
-		$('.akats16-header').toggleClass('menu-is-open');
+	$('.box-primary-nav-trigger').on('click', function(){
+		$('.box-menu-icon').toggleClass('is-clicked');
+		$('.box-header').toggleClass('menu-is-open');
 		
 		//in firefox transitions break when parent overflow is changed, so we need to wait for the end of the trasition to give the body an overflow hidden
-		if( $('.akats16-primary-nav').hasClass('is-visible') ) {
-			$('.akats16-primary-nav').removeClass('is-visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend',function(){
+		if( $('.box-primary-nav').hasClass('is-visible') ) {
+			$('.box-primary-nav').removeClass('is-visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend',function(){
 				$('body').removeClass('overflow-hidden');
 			});
 		} else {
-			$('.akats16-primary-nav').addClass('is-visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend',function(){
+			$('.box-primary-nav').addClass('is-visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend',function(){
 				$('body').addClass('overflow-hidden');
 			});	
 		}
